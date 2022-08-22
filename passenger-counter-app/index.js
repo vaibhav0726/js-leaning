ae230116182461903a74a8d32e72eb1b5f72bab0
vaibhav0726 <vaibhav.varshney2_cs20@gla.ac.in>
2022-08-23 01:53:07 +0530

    added save feature

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -2,5 +2,14 @@ let count = 0;
 
 function increment() {
   count++;
+  // pass in argument -> count
+  document.getElementById("count").innerText = count;
+}
+
+let saveEl = document.getElementById("save-el")
+
+function save(){
+  saveEl.textContent += " "+count + " - ";
+  count = 0;
   document.getElementById("count").innerText = count;
 }
